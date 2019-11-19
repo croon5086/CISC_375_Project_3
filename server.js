1f3c2dd550efa648352c14f9d7907096acd06a8f
abates1211 <47285500+abates1211@users.noreply.github.com>
2019-11-18 21:31:54 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,216 @@
+// Built-in Node.js modules
+var fs = require('fs')
+var path = require('path')
+var bodyParser = require('body-parser');
+// NPM modules
+var express = require('express');
+var sqlite3 = require('sqlite3');
+
+var public_dir = path.join(__dirname, 'public');
+var db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
+
+var app = express();
+var port = 8000;
+
+// open the database
+var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log('Error opening ' + db_filename);
+    }
+    else {
+        console.log('Now connected to ' + db_filename);
+    }
+});
+
+app.use(bodyParser.urlencoded({encoded: true}));
+
+function newIncident(req, res) {
+	return new Promise((resolve, reject) => {
+		var message = "";
+		var foundDuplicate = false;
+		var dateTime = req.body.date + "T" + req.body.time;
+
+		db.all("SELECT case_number FROM Incidents", (err, rows) => {
+			if (err) {
+				throw err;
+			}
+			for (key in rows) {
+				var a = parseInt(rows[key].case_number, 10);
+				var b = parseInt(req.body.case_number, 10);
+				if (a == b) {
+					resolve(res.status(500).send('Error, case number already exists in the database'));
+					foundDuplicate = true;
+				}
+			}
+			if (!foundDuplicate) {
+				db.all("INSERT INTO Incidents VALUES (?, ?, ?, ?, ?, ?, ?)", [req.body.case_number], [dateTime], [req.body.code], [req.body.incident], [req.body.police_grid], [req.body.neighborhood_number], [req.body.block], (err, new_rows) => {
+					if (err) {
+						console.log('error is hereree');
+						throw err;
+					}
+					resolve(res.status(200).send('Success!'));
+				});
+			}
+		});
+		
+		
+	});
+}
+
+function getIncidents(r_start_date, r_end_date, r_code, r_grid, r_neighborhood, r_limit) {
+	return new Promise((resolve, reject) => {
+		var incidents = {};
+		var sql = "SELECT case_number, date_time, code, incident, police_grid, neighborhood_number, block FROM Incidents WHERE 1=1";
+		var number_times;
+		
+		if (r_start_date != undefined) {
+			sql = sql + " AND date_time >= '" + r_start_date + "'";
+		}
+		if (r_end_date != undefined) {
+			sql = sql + " AND date_time <= '" + r_end_date + "'";
+		}
+		if (r_code != undefined) {
+			sql = sql + " AND code IN (" + r_code + ")";
+		}
+		if (r_grid != undefined) {
+			sql = sql + " AND police_grid IN (" + r_grid + ")";
+		}
+		if (r_neighborhood != undefined) {
+			sql = sql + " AND neighborhood_number IN (" + r_neighborhood + ")";
+		}
+		if (r_limit != undefined) {
+			number_times = r_limit;
+			
+		}
+		else if (r_limit == undefined) {
+			number_times = 10000;
+		}
+		sql = sql + " ORDER BY date_time DESC";
+		console.log("SQL = " + sql);
+		db.all(sql, (err, rows) => {
+			if (err) {
+				throw err;
+			}
+
+			var i = 0;
+			for(var key in rows) {
+				if(i < number_times) {
+					var currentValue = {};
+					
+					currentValue["date"] = rows[key].date_time.slice(0, 10);
+					currentValue["time"] = rows[key].date_time.slice(11, 19);
+					currentValue["code"] = rows[key].code;
+					currentValue["incident"] = rows[key].incident;
+					currentValue["police_grid"] = rows[key].police_grid;
+					currentValue["neighborhood_number"] = rows[key].neighborhood_number;
+					currentValue["block"] = rows[key].block;
+					
+					incidents["I" + rows[key].case_number] = currentValue;
+					i++;
+				}
+			}
+			
+			resolve(incidents);
+		});
+		
+	});
+}
+
+function getNeighborhoods(selected_hoods) {
+	return new Promise((resolve, reject) => {;
+		var hoods = {};
+		if (selected_hoods != undefined) {
+			var sql = "SELECT neighborhood_number, neighborhood_name FROM Neighborhoods WHERE neighborhood_number IN (" + selected_hoods + ")";
+		}
+		else {
+			var sql = "SELECT neighborhood_number, neighborhood_name FROM Neighborhoods"; 
+		}
+		db.all(sql, (err, rows) => {
+			if (err) {
+				throw err;
+			}
+			for (var key in rows) {
+				hoods["N" + rows[key].neighborhood_number] = rows[key].neighborhood_name;
+			}
+			resolve(hoods);
+		});
+	});
+}
+
+function getCodes(selected_codes) {
+	return new Promise((resolve, reject) => {	
+		var codes = {};
+		if (selected_codes != undefined) {
+			
+			var sql = "SELECT code, incident_type FROM Codes WHERE code IN (" + selected_codes + ")";
+		}
+		else {
+			var sql = "SELECT code, incident_type FROM Codes";
+		}
+		db.all(sql, (err, rows) => {
+			if (err) {
+				throw err;
+			}
+			for (var key in rows) {
+				codes["C" + rows[key].code] = rows[key].incident_type;
+			}
+			resolve(codes);
+		});
+	});
+}
+
+app.get('/codes', (req, res) => {
+	
+	Promise.all([getCodes(req.query.code)]).then((data) => {
+		if (req.query.format == 'xml') {
+			res.type('application/xml').send(data);
+		}
+		else {
+			res.type('json').send(data);
+		}
+		
+    }).catch((err) => {
+        console.log("error");
+    });
+	
+});
+
+app.get('/neighborhoods', (req, res) => {
+	Promise.all([getNeighborhoods(req.query.id)]).then((data) => {
+		if (req.query.format == 'xml') {
+			res.type('xml').send(data);
+		}
+		else {
+			res.type('json').send(data);
+		}
+    }).catch((err) => {
+        console.log("error");
+    });
+	
+});
+
+app.get('/incidents', (req, res) => {
+	Promise.all([getIncidents(req.query.start_date, req.query.end_date, req.query.code, req.query.grid, req.query.id, req.query.limit)]).then((data) => {
+		if (req.query.format == 'xml') {
+			res.type('xml').send(data);
+		}
+		else {
+			res.type('json').send(data);
+		}
+    }).catch((err) => {
+        console.log("error");
+    });
+	
+});
+
+app.put('/new-incident', (req, res) => {
+	Promise.all([newIncident(req, res)]).then((data) => {
+		
+    }).catch((err) => {
+		console.log(err);
+    });
+	
+});
+
+
+var server = app.listen(port);
